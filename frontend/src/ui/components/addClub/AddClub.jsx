7157a1f04fc3e13d28635161e2a028ac9e99a581
Nahuel <nahuel.lorenzodev@gmail.com>
2023-07-19 01:48:59 -0300

    add classname at addClub component

--- frontend/src/ui/components/addClub/AddClub.jsx
+++ frontend/src/ui/components/addClub/AddClub.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function AddClub() {
   return (
-    <Link className="btn btn-light" to="form/add">Add Club</Link>
+    <Link className="btn btn-light add-club" to="form/add">Add Club</Link>
   );
 }
 
