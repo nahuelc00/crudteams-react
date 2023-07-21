e1e8641102c0ece62276827f8d6147d3ad0276ef
Nahuel <nahuel.lorenzodev@gmail.com>
2023-07-21 02:17:28 -0300

    update paths of images

--- frontend/src/ui/components/crossClose/CrossClose.jsx
+++ frontend/src/ui/components/crossClose/CrossClose.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function CrossClose({ exitRoute }) {
   return (
     <Link to={exitRoute}>
-      <img className="cross-close" src="../../../../images/cross-close.svg" alt="cross-close" />
+      <img className="cross-close" src="/images/cross-close.svg" alt="cross-close" />
     </Link>
   );
 }
--- frontend/src/utilities/utilities.js
+++ frontend/src/utilities/utilities.js
@@ -1,6 +1,6 @@
 function handleErrorInShieldImg(event) {
   // eslint-disable-next-line no-param-reassign
-  event.target.src = '../images/not-shield.png';
+  event.target.src = '/images/not-shield.png';
 }
 
 export { handleErrorInShieldImg };
