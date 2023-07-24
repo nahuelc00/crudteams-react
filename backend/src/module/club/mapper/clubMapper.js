ae8c831a54f159dac5e8f683edc0d28e1986e2a3
Nahuel <nahuel.lorenzodev@gmail.com>
2023-07-24 19:27:36 -0300

    update name folder of mapper

---
+++ backend/src/module/club/mapper/clubMapper.js
@@ -0,0 +1,27 @@
+const Club = require('../entity/club');
+
+function mapClub(club) {
+  const newClub = new Club(club);
+
+  const clubMapped = {
+    id: newClub.id,
+    nameArea: newClub.area.name,
+    idArea: newClub.area.id,
+    name: newClub.name,
+    shortName: newClub.shortName,
+    tla: newClub.tla,
+    crestUrl: newClub.crestUrl,
+    address: newClub.address,
+    phone: newClub.phone,
+    website: newClub.website,
+    email: newClub.email,
+    founded: newClub.founded,
+    clubColors: newClub.clubColors,
+    venue: newClub.venue,
+    lastUpdated: newClub.lastUpdated,
+  };
+
+  return clubMapped;
+}
+
+module.exports = mapClub;
