c805b1f562a4e1b972df801896373e1f89759b7e
Nahuel <nahuel.lorenzodev@gmail.com>
2023-08-08 12:38:26 -0300

    add utilities

---
+++ backend/src/utilities/utilities.js
@@ -0,0 +1,33 @@
+const { getPortAndHostname } = require('../env');
+const Club = require('../module/club/entity/club');
+
+const { PORT, HOSTNAME } = getPortAndHostname();
+
+function buildClubForDB(club, imgFilename, id) {
+  return new Club(
+    {
+      id,
+      name: club.name,
+      shortname: club.shortName,
+      tla: club.tla,
+      area_name: club.nameArea,
+      area_id: Number(club.idArea),
+      crest_url: `http://${HOSTNAME}:${PORT}/files/${imgFilename}`,
+      address: club.address,
+      phone: club.phone,
+      website: club.website,
+      email: club.email,
+      founded: Number(club.founded),
+      club_colors: club.clubColors,
+      venue: club.venue,
+    },
+  );
+}
+
+function getLocalDatetime() {
+  const date = new Date().toISOString().split('T')[0];
+  const time = new Date().toTimeString().split(' ')[0];
+  return (`${date} ${time}`);
+}
+
+module.exports = { buildClubForDB, getLocalDatetime };
