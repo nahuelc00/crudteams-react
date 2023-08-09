131b59f2db10768c2ccc2be9800e595067b62f9f
Nahuel <nahuel.lorenzodev@gmail.com>
2023-08-09 20:59:16 -0300

    change return of function at build club for db

--- backend/src/utilities/utilities.js
+++ backend/src/utilities/utilities.js
@@ -1,33 +1,30 @@
 const { getPortAndHostname } = require('../env');
-const Club = require('../module/club/entity/club');
 
 const { PORT, HOSTNAME } = getPortAndHostname();
 
 function buildClubForDB(club, imgFilename, id) {
-  return new Club(
-    {
-      id,
-      name: club.name,
-      shortname: club.shortName,
-      tla: club.tla,
-      area_name: club.nameArea,
-      area_id: Number(club.idArea),
-      crest_url: `http://${HOSTNAME}:${PORT}/files/${imgFilename}`,
-      address: club.address,
-      phone: club.phone,
-      website: club.website,
-      email: club.email,
-      founded: Number(club.founded),
-      club_colors: club.clubColors,
-      venue: club.venue,
-    },
-  );
+  return {
+    id,
+    name: club.name,
+    shortname: club.shortName,
+    tla: club.tla,
+    area_name: club.nameArea,
+    area_id: Number(club.idArea),
+    crest_url: `http://${HOSTNAME}:${PORT}/files/${imgFilename}`,
+    address: club.address,
+    phone: club.phone,
+    website: club.website,
+    email: club.email,
+    founded: Number(club.founded),
+    club_colors: club.clubColors,
+    venue: club.venue,
+  };
 }
 
 function getLocalDatetime() {
   const date = new Date().toISOString().split('T')[0];
   const time = new Date().toTimeString().split(' ')[0];
-  return (`${date} ${time}`);
+  return `${date} ${time}`;
 }
 
 module.exports = { buildClubForDB, getLocalDatetime };
