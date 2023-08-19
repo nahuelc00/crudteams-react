20f2c33597e4b3c565a1fd250513c1950bcbb45b
Nahuel <nahuel.lorenzodev@gmail.com>
2023-08-19 17:25:48 -0300

    add hooks to handle save and update

---
+++ frontend/src/hooks/useHandleSaveClub.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { sendClub } from '../services/clubs';
+
+function useHandleSaveClub() {
+  const [viewModal, setViewModal] = useState(false);
+
+  function saveClub(clubFormData) {
+    sendClub(clubFormData).then(setViewModal(true));
+  }
+
+  return { viewModal, saveClub };
+}
+
+export {
+  useHandleSaveClub,
+};
---
+++ frontend/src/hooks/useHandleUpdateClub.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { updateClub } from '../services/clubs';
+
+function useHandleUpdateClub() {
+  const [viewModal, setViewModal] = useState(false);
+
+  function upgradeClub(id, clubFormData) {
+    updateClub(id, clubFormData).then(setViewModal(true));
+  }
+
+  return { viewModal, upgradeClub };
+}
+
+export {
+  useHandleUpdateClub,
+};
