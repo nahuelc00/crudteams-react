d2172ab7ff5fad6ef0ba8b4e092a37d454e6d411
Nahuel <nahuel.lorenzodev@gmail.com>
2023-08-22 11:59:08 -0300

    add callback in promise

--- frontend/src/hooks/useHandleSaveClub.js
+++ frontend/src/hooks/useHandleSaveClub.js
@@ -5,7 +5,9 @@ function useHandleSaveClub() {
   const [viewModal, setViewModal] = useState(false);
 
   function saveClub(clubFormData) {
-    sendClub(clubFormData).then(setViewModal(true));
+    sendClub(clubFormData).then(() => {
+      setViewModal(true);
+    });
   }
 
   return { viewModal, saveClub };
--- frontend/src/hooks/useHandleUpdateClub.js
+++ frontend/src/hooks/useHandleUpdateClub.js
@@ -5,7 +5,9 @@ function useHandleUpdateClub() {
   const [viewModal, setViewModal] = useState(false);
 
   function upgradeClub(id, clubFormData) {
-    updateClub(id, clubFormData).then(setViewModal(true));
+    updateClub(id, clubFormData).then(() => {
+      setViewModal(true);
+    });
   }
 
   return { viewModal, upgradeClub };
